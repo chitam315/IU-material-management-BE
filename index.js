8465193048f4eb157229eb5c9bf73dcc0bfb313c
chitam315 <nguyenlechitam2021@gmail.com>
2024-03-17 17:33:39 +0700

    fix cors second time

--- index.js
+++ index.js
@@ -9,14 +9,19 @@ import {catchError} from './app/middlewares/error.js'
 import runServerChat from "./serverChat.js";
 
 const app = express();
-const corsOption = {
-  origin: "*",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  preflightContinue: false,
-  optionsSuccessStatus: 204
-}
+// const corsOption = {
+//   origin: "*",
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204
+// }
 
-app.use(cors(corsOption))
+// app.use(cors(corsOption))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://iu-material.onrender.com/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // app.use(express.text());
 app.use(express.json());
 
