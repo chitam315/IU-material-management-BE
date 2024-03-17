5c1cd06695f71766bd6802a9bc17292ba0955da7
chitam315 <nguyenlechitam2021@gmail.com>
2024-03-17 17:57:06 +0700

    add header http ok

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use((req,res,next)=>{
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader("HTTP/1.1 200 OK")
   next();
 })
 // app.use(express.text());
