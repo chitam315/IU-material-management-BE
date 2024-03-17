88373e94662ecdc5691c60087ac15ec5788bfc94
chitam315 <nguyenlechitam2021@gmail.com>
2024-03-17 18:01:01 +0700

    fix cors

--- index.js
+++ index.js
@@ -16,16 +16,7 @@ const corsOptions = {
   
 }
 
-app.use(cors(corsOptions));
-
-app.use((req,res,next)=>{
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader("HTTP/1.1 200 OK")
-  next();
-})
-// app.use(express.text());
+app.use(express.text());
 app.use(express.json());
 
 Mongo.connect()
