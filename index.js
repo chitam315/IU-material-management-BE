b9e49c5706e41e5b4bcdbaf87b2d4ee8172a2ab9
chitam315 <nguyenlechitam2021@gmail.com>
2024-03-17 17:48:44 +0700

    add method OPTIONS for cors

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors(corsOptions));
 
 app.use((req,res,next)=>{
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 })
